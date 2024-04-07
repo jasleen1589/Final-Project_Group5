ef2f449697f3a2a847819a2da534ca6e77b5c99b
Senelli <senellijinadasa@gmail.com>
2024-04-06 22:35:48 -0700

    updated logistic regression code

---
+++ Resources/Resources/chd_table_script.sql
@@ -0,0 +1,20 @@
+CREATE TABLE CHDIndicators (
+    sex INT,
+    age INT,
+    education FLOAT,
+    currentSmoker INT,
+    cigsPerDay FLOAT,
+    BPMeds FLOAT,
+    prevalentStroke INT,
+    prevalentHyp INT,
+    diabetes INT,
+    totChol FLOAT,
+    sysBP FLOAT,
+    diaBP FLOAT,
+    BMI FLOAT,
+    heartRate FLOAT,
+    glucose FLOAT,
+    TenYearCHD INT
+);
+
+SELECT * FROM chdindicators;
